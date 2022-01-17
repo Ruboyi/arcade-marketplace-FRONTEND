eab5afd8f16ff6f5e4c1620a47af0d5e8dfa2307
aaronvt98 <aaronvt98@gmail.com>
2022-01-17 22:51:09 +0100

    ficha de producto aaron

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import TapBar from "./components/Tapbar/TapBar";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
+import ProductPage from "./pages/ProductPage/ProductPage";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/login' element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/products/:idProduct" element={<ProductPage />} />
       </Routes>
       <TapBar />
     </div>
