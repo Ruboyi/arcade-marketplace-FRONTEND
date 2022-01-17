bbde29a610f1afe4f891a7f320ce2b7b4652246d
nachhth <i.tharigen@gmail.com>
2022-01-17 19:08:46 +0100

    added template.env (BACKEND_API and PORT 3001)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { Route, Routes } from "react-router";
-import "./App.css";
-import TapBar from "./components/Tapbar/TapBar";
-import Home from "./pages/Home/Home";
-import Login from "./pages/Login/Login";
-import Register from "./pages/Register/Register";
+import { Route, Routes } from 'react-router';
+import './App.css';
+import TapBar from './components/Tapbar/TapBar';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
       <TapBar />
     </div>
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,85 +1,72 @@
-import {
-  Alert,
-  AlertTitle,
-  Button,
-  Paper,
-  Stack,
-  TextField,
-} from "@mui/material";
-import { Link } from "react-router-dom";
-import axios from "axios";
-import { Formik } from "formik";
-import { useState } from "react";
-import { useNavigate } from "react-router";
-import "./login.css";
-import logo from "../../assets/logo.jpg";
+import { Alert, AlertTitle, Button, Paper, Stack, TextField } from '@mui/material';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { Formik } from 'formik';
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
+import './login.css';
+import logo from '../../assets/logo.jpg';
 
 function Login() {
+  const { REACT_APP_BACKEND_API } = process.env;
   const navigate = useNavigate();
   const [token, setToken] = useState();
-  const [error, setError] = useState("");
+  const [error, setError] = useState('');
   return (
     <div>
       <header>
-        <img src={logo} alt="logo" />
+        <img src={logo} alt='logo' />
         <h1>Inicia sesión</h1>
       </header>
       {!token && (
-        <Paper
-          className="login-container"
-          style={{ backgroundColor: "#959CFC" }}
-        >
+        <Paper className='login-container' style={{ backgroundColor: '#959CFC' }}>
           <Formik
             initialValues={{
-              email: "",
-              password: "",
+              email: '',
+              password: ''
             }}
             validate={(values) => {
               const errors = {};
 
               if (!values.email) {
-                errors.email = "email requerido!";
+                errors.email = 'email requerido!';
               }
 
               if (!values.password) {
-                errors.password = "Contraseña requerida!";
+                errors.password = 'Contraseña requerida!';
               }
 
               return errors;
             }}
             onSubmit={async (values) => {
               try {
-                const response = await axios.post(
-                  "http://localhost:3000/api/v1/users/login",
-                  values
-                );
+                const response = await axios.post(`${REACT_APP_BACKEND_API}users/login`, values);
 
                 setToken(response.data.accessToken);
                 setTimeout(() => {
-                  navigate("/profile");
+                  navigate('/profile');
                 }, 10000);
               } catch (error) {
-                console.log("Error: ", error);
+                console.log('Error: ', error);
                 setError(error);
               }
-            }}
-          >
+            }}>
             {({
               values,
               errors,
               touched,
               handleChange,
 
-              handleSubmit,
+              handleSubmit
             }) => (
               <form onSubmit={handleSubmit}>
-                <div className="field-container">
+                <div className='field-container'>
                   <TextField
-                    id="email"
-                    name="email"
-                    type="email"
-                    label="Email"
-                    variant="standard"
+                    id='email'
+                    name='email'
+                    type='email'
+                    label='Email'
+                    variant='standard'
                     onChange={handleChange}
                     value={values.email}
                     error={errors.email && touched.email}
@@ -88,13 +75,13 @@ function Login() {
                   />
                 </div>
 
-                <div className="field-container">
+                <div className='field-container'>
                   <TextField
-                    id="password"
-                    name="password"
-                    type={"password"}
-                    label="Password"
-                    variant="standard"
+                    id='password'
+                    name='password'
+                    type={'password'}
+                    label='Password'
+                    variant='standard'
                     onChange={handleChange}
                     value={values.password}
                     error={errors.password && touched.password}
@@ -103,30 +90,28 @@ function Login() {
                   />
                 </div>
                 {error && !token && (
-                  <Stack sx={{ width: "100%" }} spacing={2}>
-                    <Alert severity="error">
+                  <Stack sx={{ width: '100%' }} spacing={2}>
+                    <Alert severity='error'>
                       <AlertTitle>Error</AlertTitle>
                       No existe un usuario con ese email y/o password !
+                      {/* FALTA EL ERROR DE QUE NO HAYA VERIFICADO SU CUENTA TODAVIA  O QUE DEL SERVIDOR NO FUNCION*/}
                     </Alert>
                   </Stack>
                 )}
-                <div className="button-submit-container">
+                <div className='button-submit-container'>
                   <Button
-                    type="submit"
-                    variant="contained"
+                    type='submit'
+                    variant='contained'
                     sx={{
-                      backgroundColor: "#3742A3",
+                      backgroundColor: '#3742A3',
                       width: 200,
                       marginBottom: 1,
-                      marginTop: 1,
-                    }}
-                  >
+                      marginTop: 1
+                    }}>
                     Login
                   </Button>
                   <div>
-                    <Link to={"/register"}>
-                      ¿Aún no tienes cuenta ? Registrate aquí!
-                    </Link>
+                    <Link to={'/register'}>¿Aún no tienes cuenta ? Registrate aquí!</Link>
                   </div>
                 </div>
               </form>
@@ -135,8 +120,8 @@ function Login() {
         </Paper>
       )}
       {token && (
-        <Stack sx={{ width: "100%" }} spacing={2}>
-          <Alert severity="success">
+        <Stack sx={{ width: '100%' }} spacing={2}>
+          <Alert severity='success'>
             <AlertTitle>Success</AlertTitle>
             Redireccionando a tu perfil.. — <strong>Logueado !!</strong>
           </Alert>
