7dfb760951f9d0d1c9318bd5b95d4455652b118f
Ruboyi <rubo2093@gmail.com>
2022-01-18 16:30:43 +0100

    modificada pagina de producto

--- src/components/ProductsGrid/ProductsGrid.js
+++ src/components/ProductsGrid/ProductsGrid.js
@@ -1,12 +1,22 @@
+import { useNavigate } from "react-router";
 import imgDefault from "../../assets/imagen_articulo_por_defecto.jpg";
 import "./ProductsGrid.css";
 
 function ProductsGrid({ products }) {
+  const navigate = useNavigate();
+  function onClickProduct(id) {
+    navigate(`/products/${id}`);
+  }
+
   return (
     <div>
       {products.map((product) => (
-        <article className="grid-product">
-          <img className="img-grid" src={imgDefault} />
+        <article
+          key={product.idProduct}
+          onClick={() => onClickProduct(product.idProduct)}
+          className="grid-product"
+        >
+          <img className="img-grid" src={imgDefault} alt="default-logo" />
           <div>
             <h1>{product.price}€</h1>
             <h2>{product.title}</h2>
