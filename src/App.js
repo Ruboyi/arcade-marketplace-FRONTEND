a4f917fda61940321ae3465f59b383c9b72d8c4e
Ruboyi <rubo2093@gmail.com>
2022-01-19 17:00:56 +0100

    corriengiendo ruta

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import { Route, Routes } from 'react-router';
-import './App.css';
-import Header from './components/Header/Header';
-import TapBar from './components/Tapbar/TapBar';
-import Home from './pages/Home/Home';
-import Login from './pages/Login/Login';
-import Products from './pages/Products/Products';
-import Register from './pages/Register/Register';
-import ProductPage from './pages/ProductPage/ProductPage';
-import Profile from './pages/Profile/Profile';
-
+import { Route, Routes } from "react-router";
+import "./App.css";
+import Header from "./components/Header/Header";
+import TapBar from "./components/Tapbar/TapBar";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Products from "./pages/Products/Products";
+import Register from "./pages/Register/Register";
+import ProductPage from "./pages/ProductPage/ProductPage";
+import UploadProduct from "./pages/UploadProducts/UploadProduct";
+import Myproducts from "./pages/MyProducts/MyProducts";
+import Profile from "./pages/Profile/Profile";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Header />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/products/:idProduct' element={<ProductPage />} />
-        <Route path='/products' element={<Products />} />
-        <Route path='/profile' element={<Profile />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/upload-product" element={<UploadProduct />} />
+        <Route path="/products/:idProduct" element={<ProductPage />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/my-products" element={<Myproducts />} />
       </Routes>
       <TapBar />
     </div>
---
+++ src/pages/MyProducts/MyProducts.js
@@ -0,0 +1,7 @@
+import "./MyProducts.css";
+
+function Myproducts() {
+  return <h1>Mis productos</h1>;
+}
+
+export default Myproducts;
