cab1dda3b71875acbd0fd1e6be473e72c013e9a9
Ruboyi <rubo2093@gmail.com>
2022-01-19 17:58:23 +0100

    MyProducts funcionando

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import { Route, Routes } from 'react-router';
-import './App.css';
-import Header from './components/Header/Header';
-import TapBar from './components/Tapbar/TapBar';
-import Home from './pages/Home/Home';
-import Login from './pages/Login/Login';
-import Products from './pages/Products/Products';
-import Register from './pages/Register/Register';
-import ProductPage from './pages/ProductPage/ProductPage';
-import Profile from './pages/Profile/Profile';
-
+import { Route, Routes } from "react-router";
+import "./App.css";
+import Header from "./components/Header/Header";
+import TapBar from "./components/Tapbar/TapBar";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Products from "./pages/Products/Products";
+import Register from "./pages/Register/Register";
+import ProductPage from "./pages/ProductPage/ProductPage";
+import Profile from "./pages/Profile/Profile";
+import Myproducts from "./pages/MyProducts/MyProducts";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Header />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/products/:idProduct' element={<ProductPage />} />
-        <Route path='/products' element={<Products />} />
-        <Route path='/profile' element={<Profile />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/products/:idProduct" element={<ProductPage />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/my-products" element={<Myproducts />} />
       </Routes>
       <TapBar />
     </div>
---
+++ src/pages/MyProducts/MyProducts.js
@@ -0,0 +1,48 @@
+import { CircularProgress } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import ProductsGrid from "../../components/ProductsGrid/ProductsGrid";
+import { useAuthorization } from "../../hooks/useAuthorization";
+import "./MyProducts.css";
+const { REACT_APP_BACKEND_API } = process.env;
+
+function Myproducts() {
+  const { userSession, userProfile } = useAuthorization();
+  const navigate = useNavigate();
+  const [myProducts, setMyProducts] = useState();
+  const { idUser } = userProfile;
+
+  useEffect(() => {
+    if (!userSession) {
+      navigate("/login");
+    }
+    async function getMyProducts() {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${userSession}`,
+        },
+      };
+      const response = await axios.get(
+        `${REACT_APP_BACKEND_API}products/user/${idUser}`,
+        config
+      );
+      console.log(response.data.data);
+      setMyProducts(response.data.data);
+    }
+    getMyProducts();
+  }, [userSession, navigate, idUser]);
+
+  return (
+    <div>
+      <h1>Mis productos</h1>
+      {myProducts ? (
+        <ProductsGrid products={myProducts} />
+      ) : (
+        <CircularProgress />
+      )}
+    </div>
+  );
+}
+
+export default Myproducts;
