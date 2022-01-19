d7ce3750ee7b5ff0b6a77ace5c9382dab0499b57
Ruboyi <rubo2093@gmail.com>
2022-01-19 15:59:12 +0100

    button añadir favoritos funcionando

--- src/components/FavoriteButton/FavoriteButton.js
+++ src/components/FavoriteButton/FavoriteButton.js
@@ -4,19 +4,25 @@ import { useAuthorization } from "../../hooks/useAuthorization";
 function FavoriteButton({ idProduct }) {
   const { userSession } = useAuthorization();
 
-  const config = {
-    headers: {
-      Authorization: `Bearer ${userSession}`,
-    },
-  };
-  return (
-    <button
-      onClick={() =>
-        axios.post(`http://localhost:3000/api/v1/products/${idProduct}`, config)
-      }
-    >
-      FAV
-    </button>
-  );
+  async function addFavorites() {
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${userSession}`,
+        },
+      };
+      const data = {};
+      const response = await axios.post(
+        `http://localhost:3000/api/v1/products/${idProduct}`,
+        data,
+        config
+      );
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return <button onClick={() => addFavorites()}>FAV</button>;
 }
 export default FavoriteButton;
