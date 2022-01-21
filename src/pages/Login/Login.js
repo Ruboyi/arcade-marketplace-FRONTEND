246f9cf253aa64c45e8c82b133aaa9151154c58a
nachhth <i.tharigen@gmail.com>
2022-01-21 20:44:12 +0100

    MyOrders page creado

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -4,73 +4,69 @@ import {
   Button,
   Paper,
   Stack,
-  TextField,
-} from "@mui/material";
-import { Link } from "react-router-dom";
-import { Formik } from "formik";
-import "./login.css";
-import logo from "../../assets/logosinfondo.png";
-import { useAuthorization } from "../../hooks/useAuthorization";
-import { useEffect } from "react";
-import { useNavigate } from "react-router";
+  TextField
+} from '@mui/material';
+import { Link } from 'react-router-dom';
+import { Formik } from 'formik';
+import './login.css';
+import logo from '../../assets/logosinfondo.png';
+import { useAuthorization } from '../../hooks/useAuthorization';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router';
 
 function Login() {
   const { login, error, userSession } = useAuthorization();
   const navigate = useNavigate();
   useEffect(() => {
     if (userSession) {
-      navigate("/profile");
+      navigate('/profile');
     }
   }, [userSession, navigate]);
   return (
     <div>
-      <header className="header-login">
-        <img className="img-login" src={logo} alt="logo" />
+      <header className='header-login'>
+        <img className='img-login' src={logo} alt='logo' />
         <h1>Inicia sesión</h1>
       </header>
       {!userSession && (
-        <Paper
-          className="login-container"
-          style={{ backgroundColor: "#959CFC" }}
-        >
+        <Paper className='login-container'>
           <Formik
             initialValues={{
-              email: "",
-              password: "",
+              email: '',
+              password: ''
             }}
             validate={(values) => {
               const errors = {};
 
               if (!values.email) {
-                errors.email = "email requerido!";
+                errors.email = 'email requerido!';
               }
 
               if (!values.password) {
-                errors.password = "Contraseña requerida!";
+                errors.password = 'Contraseña requerida!';
               }
 
               return errors;
             }}
             onSubmit={async (values) => {
               login(values.email, values.password);
-            }}
-          >
+            }}>
             {({
               values,
               errors,
               touched,
               handleChange,
 
-              handleSubmit,
+              handleSubmit
             }) => (
               <form onSubmit={handleSubmit}>
-                <div className="field-container">
+                <div className='field-container'>
                   <TextField
-                    id="email"
-                    name="email"
-                    type="email"
-                    label="Email"
-                    variant="standard"
+                    id='email'
+                    name='email'
+                    type='email'
+                    label='Email'
+                    variant='standard'
                     onChange={handleChange}
                     value={values.email}
                     error={errors.email && touched.email}
@@ -79,13 +75,13 @@ function Login() {
                   />
                 </div>
 
-                <div className="field-container">
+                <div className='field-container'>
                   <TextField
-                    id="password"
-                    name="password"
-                    type={"password"}
-                    label="Password"
-                    variant="standard"
+                    id='password'
+                    name='password'
+                    type={'password'}
+                    label='Password'
+                    variant='standard'
                     onChange={handleChange}
                     value={values.password}
                     error={errors.password && touched.password}
@@ -94,28 +90,27 @@ function Login() {
                   />
                 </div>
                 {error && (
-                  <Stack sx={{ width: "100%" }} spacing={2}>
-                    <Alert severity="error">
+                  <Stack sx={{ width: '100%' }} spacing={2}>
+                    <Alert severity='error'>
                       <AlertTitle>Error</AlertTitle>
                       {error}
                     </Alert>
                   </Stack>
                 )}
-                <div className="button-submit-container">
+                <div className='button-submit-container'>
                   <Button
-                    type="submit"
-                    variant="contained"
+                    type='submit'
+                    variant='contained'
                     sx={{
-                      backgroundColor: "#3742A3",
+                      backgroundColor: '#3742A3',
                       width: 200,
                       marginBottom: 1,
-                      marginTop: 1,
-                    }}
-                  >
+                      marginTop: 1
+                    }}>
                     Login
                   </Button>
                   <div>
-                    <Link to={"/register"}>
+                    <Link to={'/register'}>
                       ¿Aún no tienes cuenta ? Registrate aquí!
                     </Link>
                   </div>
@@ -126,8 +121,8 @@ function Login() {
         </Paper>
       )}
       {userSession && (
-        <Stack sx={{ width: "100%" }} spacing={2}>
-          <Alert severity="success">
+        <Stack sx={{ width: '100%' }} spacing={2}>
+          <Alert severity='success'>
             <AlertTitle>Success</AlertTitle>
             Redireccionando a tu perfil... — <strong>Logueado!</strong>
           </Alert>
---
+++ src/pages/MyOrders/MyOrders.js
@@ -0,0 +1,107 @@
+import { CircularProgress } from '@mui/material';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuthorization } from '../../hooks/useAuthorization';
+import './MyOrders.css';
+
+const { REACT_APP_BACKEND_API } = process.env;
+
+function MyOrders() {
+  const { userSession, userProfile } = useAuthorization();
+  const [products, setProducts] = useState();
+  const [myOrders, setMyOrders] = useState();
+  const navigate = useNavigate();
+  const { idUser } = userProfile;
+
+  useEffect(() => {
+    if (!userSession) {
+      navigate('/login');
+    }
+
+    async function getMyOrders() {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${userSession}`
+        }
+      };
+      const response = await axios.get(
+        `${REACT_APP_BACKEND_API}orders/user/${idUser}`,
+        config
+      );
+      console.log(response.data.data);
+      setMyOrders(response.data.data);
+    }
+    getMyOrders();
+
+    async function getProducts() {
+      const response = await axios.get(`${REACT_APP_BACKEND_API}products`);
+      console.log(response.data.data);
+      setProducts(response.data.data);
+    }
+    getProducts();
+  }, [userSession, navigate, idUser]);
+
+  const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  };
+
+  return (
+    <div>
+      <h1>Mis reservas</h1>
+      {myOrders ? (
+        myOrders.map((order) => {
+          let productoSolicitado = products.find(
+            (product) => product.idProduct === order.idProduct
+          );
+
+          return (
+            <div key={order.idOrder} className='order-container'>
+              <div>
+                <span>Fecha: </span>
+                <span>
+                  {new Date(order.orderDate).toLocaleString(
+                    'es-ES',
+                    options
+                  )}
+                </span>
+              </div>
+              <div className='order-product-info'>
+                {/* HACER LLAMADA A LAS IMAGENES PARA AÑADIRLA ACA */}
+                <div className='order-product-title'>
+                  {productoSolicitado.title}
+                </div>
+                <div>Precio: {productoSolicitado.price}</div>
+                <div>Ubicacion: {productoSolicitado.location}</div>
+              </div>
+              <div className='order-info'>
+                <div>Mensaje enviado: {order.orderMessage}</div>
+                {order.saleMessage && (
+                  <div>Mensaje del vendedor: {order.saleMessage}</div>
+                )}
+                {order.saleDate && (
+                  <div>
+                    Fecha acordada de venta:{' '}
+                    {new Date(order.saleDate).toLocaleString(
+                      'es-ES',
+                      options
+                    )}
+                  </div>
+                )}
+                <div className='order-status'>Estado: {order.status}</div>
+              </div>
+              {/* FALTA BOTON PARA CANCELAR RESERVA ETC */}
+            </div>
+          );
+        })
+      ) : (
+        <CircularProgress />
+      )}
+    </div>
+  );
+}
+
+export default MyOrders;
