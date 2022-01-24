1420539734112594511eaa0f09dc221ce398714f
nachhth <i.tharigen@gmail.com>
2022-01-24 17:52:31 +0100

    productsGrid img src updated

--- src/components/ProductsGrid/ProductsGrid.js
+++ src/components/ProductsGrid/ProductsGrid.js
@@ -1,35 +1,38 @@
-import { useNavigate } from "react-router";
-import imgDefault from "../../assets/imagen_articulo_por_defecto.jpg";
-import "./ProductsGrid.css";
+import { useNavigate } from 'react-router';
+// import imgDefault from '../../assets/imagen_articulo_por_defecto.jpg';
+import './ProductsGrid.css';
 
 function ProductsGrid({ products }) {
   const navigate = useNavigate();
   function onClickProduct(id) {
     navigate(`/products/${id}`);
   }
-  const 
-  isProductsAnArray = Array.isArray(products)
+  const isProductsAnArray = Array.isArray(products);
 
-  return ( <div>
-    {
-      isProductsAnArray
-     ? 
-      products.map((product) => (
-        <article
-          key={product.idProduct}
-          onClick={() => onClickProduct(product.idProduct)}
-          className="grid-product"
-        >
-          <img className="img-grid" src={imgDefault} alt="default-logo" />
-          <div>
-            <h1>{product.price}€</h1>
-            <h2>{product.title}</h2>
-            <h3>{product.category}</h3>
-            <p>{product.description}</p>
-          </div>
-        </article>
-      )) 
-     : <h1>{products}</h1>}
+  return (
+    <div>
+      {isProductsAnArray ? (
+        products.map((product) => (
+          <article
+            key={product.idProduct}
+            onClick={() => onClickProduct(product.idProduct)}
+            className='grid-product'>
+            <img
+              className='img-grid'
+              src={product.image}
+              alt='default-logo'
+            />
+            <div>
+              <h1>{product.price}€</h1>
+              <h2>{product.title}</h2>
+              <h3>{product.category}</h3>
+              <p>{product.description}</p>
+            </div>
+          </article>
+        ))
+      ) : (
+        <h1>{products}</h1>
+      )}
     </div>
   );
 }
