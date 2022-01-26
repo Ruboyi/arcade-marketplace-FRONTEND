69211dd05ef2ef6317dc7b40b9234900024a5f2a
nachhth <i.tharigen@gmail.com>
2022-01-26 22:05:12 +0100

    fixed GoBack button on profile

--- src/components/Tapbar/TapBar.js
+++ src/components/Tapbar/TapBar.js
@@ -1,43 +1,48 @@
-import { Link } from "react-router-dom";
-import "./TapBar.css";
-import Icon from "@mui/material/Icon";
-import { useAuthorization } from "../../hooks/useAuthorization";
+import { Link } from 'react-router-dom';
+import './TapBar.css';
+import Icon from '@mui/material/Icon';
+import { useAuthorization } from '../../hooks/useAuthorization';
 
 function TapBar() {
-  const { userProfile } = useAuthorization();
+  const { userProfile, userSession } = useAuthorization();
 
   return (
-    <nav className="tapBar-navigation">
+    <nav className='tapBar-navigation'>
       <div>
-        <Link to={"/"}>
-          <Icon className={"home-icon"}>home</Icon>
+        <Link to={'/'}>
+          <Icon className={'home-icon'}>home</Icon>
         </Link>
       </div>
       <div>
-        {" "}
-        <Link to={"/my-favorites"}>
-          <Icon className={"favorite-icon"}>favorite</Icon>
+        <Link to={'/my-favorites'}>
+          <Icon className={'favorite-icon'}>favorite</Icon>
         </Link>
       </div>
       <div>
-        {" "}
-        <Link to={"/upload-product"}>
-          <Icon className={"add-icon"}>add_circle</Icon>
+        <Link to={'/upload-product'}>
+          <Icon className={'add-icon'}>add_circle</Icon>
         </Link>
       </div>
       <div>
-        <Link to={"/login"}>
-          {userProfile && userProfile.image ? (
-            <img
-              className="img-tapbar"
-              src={userProfile.image}
-              alt="profile"
-              height={40}
-            />
-          ) : (
-            <Icon className={"login-icon"}>account_circle</Icon>
-          )}
-        </Link>
+        {!userSession ? (
+          <Link to={'/login'}>
+            {' '}
+            <Icon className={'login-icon'}>account_circle</Icon>{' '}
+          </Link>
+        ) : (
+          <Link to={'/profile'}>
+            {userProfile && userProfile.image ? (
+              <img
+                className='img-tapbar'
+                src={userProfile.image}
+                alt='profile'
+                height={40}
+              />
+            ) : (
+              <Icon className={'login-icon'}>account_circle</Icon>
+            )}
+          </Link>
+        )}
       </div>
     </nav>
   );
