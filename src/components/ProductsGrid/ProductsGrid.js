0ec6fa1125f0f9b435386508f9ee00a30780a68d
Ruboyi <rubo2093@gmail.com>
2022-01-31 13:41:31 +0100

    cambiado products grip

--- src/components/ProductsGrid/ProductsGrid.js
+++ src/components/ProductsGrid/ProductsGrid.js
@@ -1,24 +1,23 @@
-
-import { useNavigate } from 'react-router';
+import { Paper } from "@mui/material";
+import { useNavigate } from "react-router";
+import FavoriteButton from "../FavoriteButton/FavoriteButton";
 
 // import imgDefault from '../../assets/imagen_articulo_por_defecto.jpg';
-import './ProductsGrid.css';
+import "./ProductsGrid.css";
 
 function ProductsGrid({ products }) {
-
   const navigate = useNavigate();
-  function onClickProduct(id) {
-    navigate(`/products/${id}`);
-  }
 
-  let productsProcessed = products
-  const actualUrl = window.location.href
+  let productsProcessed = products;
+  const actualUrl = window.location.href;
 
-  if (actualUrl.includes('search')) {
-    const position = actualUrl.search('search=') + 7
-    const search = actualUrl.slice(position)
+  if (actualUrl.includes("search")) {
+    const position = actualUrl.search("search=") + 7;
+    const search = actualUrl.slice(position);
     console.log(search);
-    productsProcessed = products.filter(product => product.title.toLowerCase().includes(search.toLocaleLowerCase()))
+    productsProcessed = products.filter((product) =>
+      product.title.toLowerCase().includes(search.toLocaleLowerCase())
+    );
     console.log(productsProcessed);
   }
 
@@ -26,28 +25,35 @@ function ProductsGrid({ products }) {
 
   return (
     <div>
-      {isProductsAnArray ? (
-        productsProcessed.map((product) => (
-          <article
-            key={product.idProduct}
-            onClick={() => onClickProduct(product.idProduct)}
-            className='grid-product'>
-            <img
-              className='img-grid'
-              src={product.images[0]}
-              alt='default-logo'
-            />
-            <div>
-              <h1>{product.price}€</h1>
-              <h2>{product.title}</h2>
-              <h3>{product.category}</h3>
-              <p>{product.description}</p>
-            </div>
-          </article>
-        ))
-      ) : (
-        <h1>{products}</h1>
-      )}
+      <Paper className="productGrid-card-container">
+        {isProductsAnArray ? (
+          productsProcessed.map((product) => (
+            <Paper
+              key={product.idProduct}
+              elevation={6}
+              className="product-card"
+            >
+              <img
+                onClick={() => navigate(`/products/${product.idProduct}`)}
+                src={product.images[0]}
+                alt="product-img"
+                width="180vh"
+                height="150px"
+              />
+              <div className="product-card-header">
+                <h2 className="title-product-card">{product.price}€</h2>
+                <FavoriteButton
+                  className="favorite-button-product-card"
+                  idProduct={product.idProduct}
+                />
+              </div>
+              <p>{product.title}</p>
+            </Paper>
+          ))
+        ) : (
+          <h1>{products}</h1>
+        )}
+      </Paper>
     </div>
   );
 }
