1e740e9a11e19b2cbe8712d9a01b53b650a3e0fe
Ruboyi <rubo2093@gmail.com>
2022-02-05 00:32:11 +0100

    googleMaps fixed

--- src/components/GoogleMap/GoogleMap.js
+++ src/components/GoogleMap/GoogleMap.js
@@ -1,29 +1,53 @@
+import axios from "axios";
 import GoogleMapReact from "google-map-react";
+import { useEffect, useState } from "react";
 
+const { REACT_APP_GOOGLE_MAP_KEY } = process.env;
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-export default function GoogleMap({ coords }) {
+export default function GoogleMap({ location }) {
+  const [coords, setCoords] = useState(false);
+
+  useEffect(() => {
+    async function getGeolocatationProduct() {
+      try {
+        console.log(location);
+        const response = await axios.get(
+          `https://maps.googleapis.com/maps/api/geocode/json?key=${REACT_APP_GOOGLE_MAP_KEY}&address=${location}spain`
+        );
+        setCoords(response.data.results[0].geometry.location);
+        console.log(response.data.results[0].geometry.location);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getGeolocatationProduct();
+  }, [location]);
   const { lat, lng } = coords;
 
   const defaultProps = {
     center: {
-      lat: 43.3715,
-      lng: -8.39597,
+      lat: lat,
+      lng: lng,
     },
     zoom: 13,
   };
 
   return (
-    <div style={{ height: "300px", width: "100%" }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{
-          key: "AIzaSyCDc2MrK0NHh3Sm9xakYDaa7VUpvCzl3UM",
-        }}
-        defaultCenter={defaultProps.center}
-        defaultZoom={defaultProps.zoom}
-      >
-        <AnyReactComponent lat={lat} lng={lng} text="Aquí" />
-      </GoogleMapReact>
-    </div>
+    <>
+      {coords && (
+        <div style={{ height: "300px", width: "100%" }}>
+          <GoogleMapReact
+            bootstrapURLKeys={{
+              key: "AIzaSyCDc2MrK0NHh3Sm9xakYDaa7VUpvCzl3UM",
+            }}
+            defaultCenter={defaultProps.center}
+            defaultZoom={defaultProps.zoom}
+          >
+            <AnyReactComponent lat={lat} lng={lng} text="Aquí" />
+          </GoogleMapReact>
+        </div>
+      )}
+    </>
   );
 }
--- src/pages/UploadProducts/UploadProduct.js
+++ src/pages/UploadProducts/UploadProduct.js
@@ -9,18 +9,18 @@ import {
   InputAdornment,
   Stack,
   Alert,
-  AlertTitle
-} from '@mui/material';
-import InfoIcon from '@mui/icons-material/Info';
-import ListIcon from '@mui/icons-material/List';
-import AddLocationIcon from '@mui/icons-material/AddLocation';
-import './upload-products.css';
-import { Formik, Field } from 'formik';
-import axios from 'axios';
-import { useNavigate } from 'react-router';
-import { useEffect, useState } from 'react';
-import { useAuthorization } from '../../hooks/useAuthorization';
-import GoBack from '../../components/GoBack/GoBack';
+  AlertTitle,
+} from "@mui/material";
+import InfoIcon from "@mui/icons-material/Info";
+import ListIcon from "@mui/icons-material/List";
+import AddLocationIcon from "@mui/icons-material/AddLocation";
+import "./upload-products.css";
+import { Formik, Field } from "formik";
+import axios from "axios";
+import { useNavigate } from "react-router";
+import { useEffect, useState } from "react";
+import { useAuthorization } from "../../hooks/useAuthorization";
+import GoBack from "../../components/GoBack/GoBack";
 
 const { REACT_APP_BACKEND_API } = process.env;
 
@@ -29,90 +29,85 @@ function UploadProduct() {
   const navigate = useNavigate();
   const { userSession } = useAuthorization();
   const [error, setError] = useState();
-  // const [preview, setPreview] = useState();
+  const [preview, setPreview] = useState();
 
   console.log(fichero);
 
   useEffect(() => {
     if (!userSession) {
-      navigate('/login');
+      navigate("/login");
     }
   }, [userSession, navigate]);
 
-  // useEffect(() => {
-  //   if (fichero) {
-  //     const reader = new FileReader();
-  //     reader.onloadend = () => {
-  //       setPreview(reader.result);
-  //     };
-  //     reader.readAsDataURL(fichero);
-  //   } else {
-  //     setPreview(null);
-  //   }
-  // }, [fichero]);
+  useEffect(() => {
+    if (fichero) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreview(reader.result);
+      };
+      reader.readAsDataURL(fichero);
+    } else {
+      setPreview(null);
+    }
+  }, [fichero]);
   console.log(fichero);
 
   return (
     <main>
       <GoBack />
       <Paper
-        className='upload-product-form'
-        style={{ backgroundColor: 'white', marginTop: '20px' }}>
+        className="upload-product-form"
+        style={{ backgroundColor: "white", marginTop: "20px" }}
+      >
         <Formik
           initialValues={{
-            category: '',
-            title: '',
-            description: '',
-            price: '',
-            state: '',
-            location: ''
+            category: "",
+            title: "",
+            description: "",
+            price: "",
+            state: "",
+            location: "",
           }}
           validate={(values) => {
             const errors = {};
 
             if (!values.title) {
-              errors.title = 'Título Required';
+              errors.title = "Título Required";
             }
             if (!values.description) {
-              errors.description = 'description Required';
+              errors.description = "description Required";
             }
             if (!values.price) {
-              errors.price = 'price Required';
+              errors.price = "price Required";
             }
             if (!values.state) {
-              errors.state = 'state Required';
+              errors.state = "state Required";
             }
             if (!values.location) {
-              errors.location = 'location Required';
+              errors.location = "location Required";
             }
             return errors;
           }}
           onSubmit={async (values) => {
-            console.log('SUBMIT: ', values);
-            const {
-              category,
-              title,
-              description,
-              price,
-              state,
-              location
-            } = values;
+            console.log("SUBMIT: ", values);
+            const { category, title, description, price, state, location } =
+              values;
 
             try {
               const config = {
                 headers: {
-                  Authorization: `Bearer ${userSession}`
-                }
+                  Authorization: `Bearer ${userSession}`,
+                },
               };
               const response = await axios.post(
-                'http://localhost:3000/api/v1/products/',
+                "http://localhost:3000/api/v1/products/",
                 {
                   category,
                   title,
                   description,
                   price,
                   state,
-                  location
+                  location,
                 },
                 config
               );
@@ -120,10 +115,8 @@ function UploadProduct() {
               const { productId } = response.data;
 
               const formData = new FormData();
-              const newFichero = [...fichero];
-              console.log(newFichero);
 
-              formData.append('productImage', newFichero);
+              formData.append("productImage", fichero);
 
               await axios.post(
                 `${REACT_APP_BACKEND_API}products/images/${productId}`,
@@ -132,49 +125,45 @@ function UploadProduct() {
               );
 
               setTimeout(() => {
-                navigate('/products');
+                navigate("/products");
               }, 10000);
             } catch (error) {
               setError(error.response.data.error);
             }
-          }}>
+          }}
+        >
           {/* TODO Radials no funcionan */}
           {({ values, errors, touched, handleChange, handleSubmit }) => (
             <form onSubmit={handleSubmit}>
               <h2>
-                Categoría <ListIcon />{' '}
+                Categoría <ListIcon />{" "}
               </h2>
               <div>
                 <label>
-                  <Field type='radio' name='category' value='consolas' />{' '}
+                  <Field type="radio" name="category" value="consolas" />{" "}
                   Consola
                 </label>
                 <label>
-                  <Field type='radio' name='category' value='arcades' />{' '}
-                  Arcades
+                  <Field type="radio" name="category" value="arcades" /> Arcades
                 </label>
                 <label>
-                  <Field
-                    type='radio'
-                    name='category'
-                    value='videojuegos'
-                  />{' '}
+                  <Field type="radio" name="category" value="videojuegos" />{" "}
                   Videojuegos
                 </label>
                 <label>
-                  <Field type='radio' name='category' value='accesorios' />{' '}
+                  <Field type="radio" name="category" value="accesorios" />{" "}
                   Accesorios
                 </label>
               </div>
               <h2>Información</h2> <InfoIcon />
-              <div className='titulo-precio'>
+              <div className="titulo-precio">
                 <TextField
-                  margin='dense'
+                  margin="dense"
                   sx={{ marginRight: 1 }}
-                  className='titulo-producto'
-                  id='title'
-                  label='Título'
-                  variant='outlined'
+                  className="titulo-producto"
+                  id="title"
+                  label="Título"
+                  variant="outlined"
                   onChange={handleChange}
                   value={values.title}
                   error={errors.title && touched.title}
@@ -182,10 +171,10 @@ function UploadProduct() {
                 />
 
                 <TextField
-                  margin='dense'
-                  id='price'
-                  label='Precio (€)'
-                  variant='outlined'
+                  margin="dense"
+                  id="price"
+                  label="Precio (€)"
+                  variant="outlined"
                   onChange={handleChange}
                   value={values.price}
                   error={errors.price && touched.price}
@@ -200,12 +189,12 @@ function UploadProduct() {
               errors.location} */}
               <div>
                 <TextField
-                  margin='dense'
-                  id='description'
-                  label='Descripción del producto'
+                  margin="dense"
+                  id="description"
+                  label="Descripción del producto"
                   multiline
                   rows={4}
-                  variant='outlined'
+                  variant="outlined"
                   onChange={handleChange}
                   value={values.description}
                   error={errors.description && touched.description}
@@ -213,31 +202,33 @@ function UploadProduct() {
                   fullWidth
                 />
               </div>
-              <div className='estado-localidad'>
+              <div className="estado-localidad">
                 <FormControl
                   fullWidth
-                  margin='dense'
+                  margin="dense"
                   sx={{ marginRight: 1 }}
-                  variant='outlined'>
-                  <InputLabel id='state'>Estado</InputLabel>
+                  variant="outlined"
+                >
+                  <InputLabel id="state">Estado</InputLabel>
 
                   <Select
-                    className='estado'
+                    className="estado"
                     value={values.state}
-                    id='state'
-                    name='state'
-                    label='Estado'
-                    variant='standard'
+                    id="state"
+                    name="state"
+                    label="Estado"
+                    variant="standard"
                     onChange={handleChange}
                     error={errors.state && touched.state}
-                    state>
-                    <MenuItem value='' disabled>
+                    state
+                  >
+                    <MenuItem value="" disabled>
                       Selecciona el estado del producto
                     </MenuItem>
-                    <MenuItem value={'nuevo'}>Nuevo</MenuItem>
-                    <MenuItem value={'seminuevo'}>Seminuevo</MenuItem>
+                    <MenuItem value={"nuevo"}>Nuevo</MenuItem>
+                    <MenuItem value={"seminuevo"}>Seminuevo</MenuItem>
                     {/* <MenuItem value={"buen estado"}>Buen estado</MenuItem> */}
-                    <MenuItem value={'usado'}>Usado</MenuItem>
+                    <MenuItem value={"usado"}>Usado</MenuItem>
                     {/* <MenuItem value={"Malas condiciones"}>
                     Malas condiciones
                   </MenuItem> */}
@@ -245,20 +236,20 @@ function UploadProduct() {
                 </FormControl>
 
                 <TextField
-                  margin='dense'
-                  id='location'
-                  label='Localidad'
-                  variant='outlined'
+                  margin="dense"
+                  id="location"
+                  label="Localidad"
+                  variant="outlined"
                   onChange={handleChange}
                   value={values.location}
                   error={errors.location && touched.location}
                   helperText={touched.location && errors.location}
                   InputProps={{
                     endAdornment: (
-                      <InputAdornment position='end'>
+                      <InputAdornment position="end">
                         <AddLocationIcon />
                       </InputAdornment>
-                    )
+                    ),
                   }}
                 />
               </div>
@@ -272,54 +263,49 @@ function UploadProduct() {
                 <h2>Subir imagen</h2>
                 <div>
                   <label>
-                    Imagen:{' '}
+                    Imagen:{" "}
                     <input
                       required
                       multiple
-                      accept='image/*'
-                      type={'file'}
+                      accept="image/*"
+                      type={"file"}
                       onChange={(event) => {
-                        // console.log(event.target.files);
-                        // console.log(event.target.files[0]);
-                        const file = event.target.files;
-                        // if (file && file.type.substr(0, 5) === "image") {
-                        //   setFichero(file);
-                        // } else {
-                        //   setFichero(null);
-                        // }
-                        console.log(file);
+                        const file = event.target.files[0];
+                        if (file && file.type.substr(0, 5) === "image") {
+                          setFichero(file);
+                        } else {
+                          setFichero(null);
+                        }
+
                         setFichero(file);
                       }}
                     />
                   </label>
                   {fichero && (
                     <div>
-                      <img
-                        className='img-preview'
-                        // src={preview}
-                        alt='img'
-                      />
+                      <img className="img-preview" src={preview} alt="img" />
                     </div>
                   )}
                 </div>
               </div>
               {error && (
-                <Stack sx={{ width: '100%', margin: 1 }} spacing={2}>
-                  <Alert severity='error'>
+                <Stack sx={{ width: "100%", margin: 1 }} spacing={2}>
+                  <Alert severity="error">
                     <AlertTitle>Error</AlertTitle>
                     {error}
                   </Alert>
                 </Stack>
               )}
               <Button
-                type='submit'
-                variant='contained'
+                type="submit"
+                variant="contained"
                 sx={{
-                  backgroundColor: '#3742A3',
+                  backgroundColor: "#3742A3",
                   width: 200,
                   marginBottom: 1,
-                  marginTop: 2
-                }}>
+                  marginTop: 2,
+                }}
+              >
                 Publicar
               </Button>
             </form>
