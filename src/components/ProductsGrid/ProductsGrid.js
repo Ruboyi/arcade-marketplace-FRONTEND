084402e3671861a3a387b8f0ba86a69e17dafb71
Ruboyi <rubo2093@gmail.com>
2022-02-14 15:02:14 +0100

    arreglado filtrar productos

--- src/components/ProductsGrid/ProductsGrid.js
+++ src/components/ProductsGrid/ProductsGrid.js
@@ -8,66 +8,92 @@ function ProductsGrid({ products }) {
   let productsProcessed = products;
   const actualUrl = window.location.href;
 
-  const position = actualUrl.search('/products')
-  const slicedUrl = actualUrl.slice(position + 9)
-  if (slicedUrl.includes('&')) {
-
-    const replacedUrl = slicedUrl.replace('?', '')
-    const splitedUrl = replacedUrl.split('&')
-    const formattedQueries = {}
-    splitedUrl.forEach(query => {
-      const [key, value] = query.split('=')
-      Object.assign(formattedQueries, { [key]: value })
+  const position = actualUrl.search("/products");
+  const slicedUrl = actualUrl.slice(position + 9);
+  if (slicedUrl.includes("&")) {
+    const replacedUrl = slicedUrl.replace("?", "");
+    const splitedUrl = replacedUrl.split("&");
+    const formattedQueries = {};
+    splitedUrl.forEach((query) => {
+      const [key, value] = query.split("=");
+      Object.assign(formattedQueries, { [key]: value });
       console.log(formattedQueries);
-    })
+    });
 
-    if (formattedQueries['category'] && formattedQueries['category'] !== 'undefined') {
-      productsProcessed = productsProcessed.filter((product) =>
-        product.category === formattedQueries['category'])
+    if (
+      formattedQueries["category"] &&
+      formattedQueries["category"] !== "undefined"
+    ) {
+      productsProcessed = productsProcessed.filter(
+        (product) => product.category === formattedQueries["category"]
+      );
     }
 
-    if (formattedQueries['search'] && formattedQueries['search'] !== 'undefined') {
+    if (
+      formattedQueries["search"] &&
+      formattedQueries["search"] !== "undefined"
+    ) {
       productsProcessed = productsProcessed.filter((product) =>
-        product.title.toLowerCase().includes(formattedQueries['search'].toLocaleLowerCase()))
+        product.title
+          .toLowerCase()
+          .includes(formattedQueries["search"].toLocaleLowerCase())
+      );
       console.log(productsProcessed);
     }
 
-    if (formattedQueries['lowPrice'] && formattedQueries['lowPrice'] !== 'undefined') {
-      productsProcessed = productsProcessed.filter((product) =>
-        product.price >= Number(formattedQueries['lowPrice']))
+    if (
+      formattedQueries["lowPrice"] &&
+      formattedQueries["lowPrice"] !== "undefined"
+    ) {
+      productsProcessed = productsProcessed.filter(
+        (product) => product.price >= Number(formattedQueries["lowPrice"])
+      );
       console.log(productsProcessed);
     }
 
-    if (formattedQueries['highPrice'] && formattedQueries['highPrice'] !== 'undefined') {
-      productsProcessed = productsProcessed.filter((product) =>
-        product.price <= Number(formattedQueries['highPrice']))
+    if (
+      formattedQueries["highPrice"] &&
+      formattedQueries["highPrice"] !== "undefined"
+    ) {
+      productsProcessed = productsProcessed.filter(
+        (product) => product.price <= Number(formattedQueries["highPrice"])
+      );
     }
-    if (formattedQueries['province'] && formattedQueries['province'] !== 'undefined') {
-      productsProcessed = productsProcessed.filter((product) =>
-        product.province === formattedQueries['province'])
+    if (
+      formattedQueries["province"] &&
+      formattedQueries["province"] !== "undefined"
+    ) {
+      productsProcessed = productsProcessed.filter(
+        (product) => product.province === formattedQueries["province"]
+      );
     }
-    if (formattedQueries['status'] && formattedQueries['status'] !== 'undefined') {
-      productsProcessed = productsProcessed.filter((product) =>
-        product.state === formattedQueries['status'])
+    if (
+      formattedQueries["status"] &&
+      formattedQueries["status"] !== "undefined"
+    ) {
+      productsProcessed = productsProcessed.filter(
+        (product) => product.state === formattedQueries["status"]
+      );
     }
-
-  } else if (!slicedUrl.includes('&') && slicedUrl.includes('?')) {
-
-    const replacedUrl = slicedUrl.replace('?', '')
-    const formattedQueries = {}
-    const [key, value] = replacedUrl.split('=')
-    Object.assign(formattedQueries, { [key]: value })
+  } else if (!slicedUrl.includes("&") && slicedUrl.includes("?")) {
+    const replacedUrl = slicedUrl.replace("?", "");
+    const formattedQueries = {};
+    const [key, value] = replacedUrl.split("=");
+    Object.assign(formattedQueries, { [key]: value });
     console.log(formattedQueries);
 
-    if (formattedQueries['category']) {
-      productsProcessed = productsProcessed.filter((product) =>
-        product.category === formattedQueries['category'])
+    if (formattedQueries["category"]) {
+      productsProcessed = productsProcessed.filter(
+        (product) => product.category === formattedQueries["category"]
+      );
     }
-    if (formattedQueries['search']) {
+    if (formattedQueries["search"]) {
       productsProcessed = productsProcessed.filter((product) =>
-        product.title.toLowerCase().includes(formattedQueries['search'].toLocaleLowerCase()))
+        product.title
+          .toLowerCase()
+          .includes(formattedQueries["search"].toLocaleLowerCase())
+      );
     }
-
   }
 
   //! OLD CODE
@@ -100,13 +126,13 @@ function ProductsGrid({ products }) {
 
   return (
     <div className="productGrid-card-container">
-
       {isProductsAnArray ? (
-        productsProcessed.map((product) => <ProductCard key={product.idProduct} product={product} />)
+        productsProcessed.map((product) => (
+          <ProductCard key={product.idProduct} product={product} />
+        ))
       ) : (
         <CircularProgress />
       )}
-
     </div>
   );
 }
--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -8,95 +8,101 @@ import consolasLogo from "../../assets/gameboy.png";
 import videojuegosLogo from "../../assets/cd.png";
 import accesoriosLogo from "../../assets/gamepad.png";
 import arcadesLogo from "../../assets/arcade.png";
-import './Products.css'
+import "./Products.css";
 
 function Products() {
   const [products, setProducts] = useState("");
-  const [splitedUrl, setSplitedUrl] = useState()
-  const navigate = useNavigate()
-  const url = window.location.href
-  const [lowPrice, setLowPrice] = useState()
-  const [highPrice, setHighPrice] = useState()
-  const [province, setProvince] = useState()
-  const [status, setStatus] = useState()
-
+  const [splitedUrl, setSplitedUrl] = useState();
+  const navigate = useNavigate();
+  const url = window.location.href;
+  const [lowPrice, setLowPrice] = useState();
+  const [highPrice, setHighPrice] = useState();
+  const [province, setProvince] = useState();
+  const [status, setStatus] = useState();
 
   useEffect(() => {
     async function getData() {
       const products = await getProducts();
       setProducts(products);
     }
-    const position = window.location.href.search('products')
-    if (window.location.href.includes('&')) {
-      const preSplitUrl = window.location.href.split('&')
-      setSplitedUrl(`${preSplitUrl[0].slice(position + 8)}`)
+    const position = window.location.href.search("products");
+    if (window.location.href.includes("&")) {
+      const preSplitUrl = window.location.href.split("&");
+      setSplitedUrl(`${preSplitUrl[0].slice(position + 8)}`);
     } else {
-      setSplitedUrl(`${window.location.href.slice(position + 8)}`)
+      setSplitedUrl(`${window.location.href.slice(position + 8)}`);
     }
     getData();
   }, [url]);
 
   return (
-    <><Paper elevation={2} sx={{ maxHeight: '200px', position: 'fixed' }}>
-      <nav className="categories">
-        <div>
-          <a href="/products?category=consolas">
-            <img src={consolasLogo} alt="consolas logo"></img>Consolas
-          </a>
-        </div>
-        <div>
-          <a href="/products?category=videojuegos">
-            <img src={videojuegosLogo} alt="videojuegos logo"></img>
-            Videojuegos
-          </a>
-        </div>
-        <div>
-          <a href="/products?category=accesorios">
-            <img src={accesoriosLogo} alt="accesorios logo"></img>Accesorios
-          </a>
-        </div>
-        <div>
-          <a href="/products?category=arcades">
-            {" "}
-            <img src={arcadesLogo} alt="arcades logo"></img>Arcades
-          </a>
-        </div>
-      </nav>
-      <div className="divFilters">
-        <input placeholder="precio minimo SOLO NUMEROS"
-          onChange={(event) => {
-            let priceData = event.target.value;
-            if (priceData) { setLowPrice(priceData); }
-          }
-          }
-        />
-        <input placeholder="precio maximo SOLO NUMEROS"
-          onChange={(event) => {
-            let priceData = event.target.value;
-            if (priceData) { setHighPrice(priceData); }
-          }
-          }
-        />
-        <input placeholder="provincia SELECT"
-          onChange={(event) => {
-            let provinceData = event.target.value;
-            if (provinceData) { setProvince(provinceData); }
-          }
-          }
-        />
-        <input placeholder="estado SELECT"
-          onChange={(event) => {
-            let statusData = event.target.value;
-            if (statusData) { setStatus(statusData); }
-          }
-          }
-        />
+    <>
+      <Paper elevation={2} sx={{ maxHeight: "200px", position: "fixed" }}>
+        <nav className="categories">
+          <div>
+            <a href="/products?category=consolas">
+              <img src={consolasLogo} alt="consolas logo"></img>Consolas
+            </a>
+          </div>
+          <div>
+            <a href="/products?category=videojuegos">
+              <img src={videojuegosLogo} alt="videojuegos logo"></img>
+              Videojuegos
+            </a>
+          </div>
+          <div>
+            <a href="/products?category=accesorios">
+              <img src={accesoriosLogo} alt="accesorios logo"></img>Accesorios
+            </a>
+          </div>
+          <div>
+            <a href="/products?category=arcades">
+              {" "}
+              <img src={arcadesLogo} alt="arcades logo"></img>Arcades
+            </a>
+          </div>
+        </nav>
+        <div className="divFilters">
+          <input
+            placeholder="precio minimo SOLO NUMEROS"
+            onChange={(event) => {
+              let priceData = event.target.value;
+              setLowPrice(priceData);
+            }}
+          />
+          <input
+            placeholder="precio maximo SOLO NUMEROS"
+            onChange={(event) => {
+              let priceData = event.target.value;
+              setHighPrice(priceData);
+            }}
+          />
+          <input
+            placeholder="provincia SELECT"
+            onChange={(event) => {
+              let provinceData = event.target.value;
+              setProvince(provinceData);
+            }}
+          />
+          <input
+            placeholder="estado SELECT"
+            onChange={(event) => {
+              let statusData = event.target.value;
+              setStatus(statusData);
+            }}
+          />
 
-        <button onClick={() => navigate(`/products${splitedUrl}&lowPrice=${lowPrice}&highPrice=${highPrice}&province=${province}&status=${status}`)}>
-          Filtrar
-        </button>
-      </div>
-    </Paper>
+          <button
+            onClick={() =>
+              navigate(
+                `/products${splitedUrl}&lowPrice=${lowPrice}&highPrice=${highPrice}&province=${province}&status=${status}`
+              )
+            }
+          >
+            Filtrar
+          </button>
+        </div>
+      </Paper>
       <div className="productsGrid">
         {products ? <ProductsGrid products={products} /> : <CircularProgress />}
       </div>
