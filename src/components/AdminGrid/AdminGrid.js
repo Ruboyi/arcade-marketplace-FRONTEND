82c8c52ba861c7829b4b65506ca5432cd13fd1f1
Ruboyi <rubo2093@gmail.com>
2022-02-17 18:16:22 +0100

    añadido Datagrid

--- src/components/AdminGrid/AdminGrid.js
+++ src/components/AdminGrid/AdminGrid.js
@@ -1,234 +1,31 @@
 import * as React from "react";
-import PropTypes from "prop-types";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import AddIcon from "@mui/icons-material/Add";
-import EditIcon from "@mui/icons-material/Edit";
-import DeleteIcon from "@mui/icons-material/DeleteOutlined";
-import SaveIcon from "@mui/icons-material/Save";
-import CancelIcon from "@mui/icons-material/Close";
-import {
-  useGridApiRef,
-  DataGridPro,
-  GridToolbarContainer,
-  GridActionsCellItem,
-} from "@mui/x-data-grid-pro";
-import {
-  randomCreatedDate,
-  randomTraderName,
-  randomUpdatedDate,
-  randomId,
-} from "@mui/x-data-grid-generator";
+import { DataGrid } from "@mui/x-data-grid";
 import { Paper } from "@mui/material";
 
-function EditToolbar(props) {
-  const { apiRef } = props;
-
-  const handleClick = () => {
-    const id = randomId();
-    apiRef.current.updateRows([{ id, isNew: true }]);
-    apiRef.current.setRowMode(id, "edit");
-    // Wait for the grid to render with the new row
-    setTimeout(() => {
-      apiRef.current.scrollToIndexes({
-        rowIndex: apiRef.current.getRowsCount() - 1,
-      });
-
-      apiRef.current.setCellFocus(id, "name");
-    });
-  };
-
-  return (
-    <GridToolbarContainer>
-      <Button color="primary" startIcon={<AddIcon />} onClick={handleClick}>
-        Add record
-      </Button>
-    </GridToolbarContainer>
-  );
-}
-
-EditToolbar.propTypes = {
-  apiRef: PropTypes.shape({
-    current: PropTypes.object.isRequired,
-  }).isRequired,
-};
-
-export default function AdminGrid(usersData) {
-  const apiRef = useGridApiRef();
-
-  const rows = [
-    {
-      id: randomId(),
-      name: randomTraderName(),
-      age: 25,
-      dateCreated: randomCreatedDate(),
-      lastLogin: randomUpdatedDate(),
-    },
-    {
-      id: randomId(),
-      name: randomTraderName(),
-      age: 36,
-      dateCreated: randomCreatedDate(),
-      lastLogin: randomUpdatedDate(),
-    },
-    {
-      id: randomId(),
-      name: randomTraderName(),
-      age: 19,
-      dateCreated: randomCreatedDate(),
-      lastLogin: randomUpdatedDate(),
-    },
-    {
-      id: randomId(),
-      name: randomTraderName(),
-      age: 28,
-      dateCreated: randomCreatedDate(),
-      lastLogin: randomUpdatedDate(),
-    },
-    {
-      id: randomId(),
-      name: randomTraderName(),
-      age: 23,
-      dateCreated: randomCreatedDate(),
-      lastLogin: randomUpdatedDate(),
-    },
-  ];
-
-  const handleRowEditStart = (params, event) => {
-    event.defaultMuiPrevented = true;
-  };
-
-  const handleRowEditStop = (params, event) => {
-    event.defaultMuiPrevented = true;
-  };
-
-  const handleCellFocusOut = (params, event) => {
-    event.defaultMuiPrevented = true;
-  };
-
-  const handleEditClick = (id) => (event) => {
-    event.stopPropagation();
-    apiRef.current.setRowMode(id, "edit");
-  };
-
-  const handleSaveClick = (id) => async (event) => {
-    event.stopPropagation();
-    // Wait for the validation to run
-    const isValid = await apiRef.current.commitRowChange(id);
-    if (isValid) {
-      apiRef.current.setRowMode(id, "view");
-      const row = apiRef.current.getRow(id);
-      apiRef.current.updateRows([{ ...row, isNew: false }]);
-    }
-  };
-
-  const handleDeleteClick = (id) => (event) => {
-    event.stopPropagation();
-    apiRef.current.updateRows([{ id, _action: "delete" }]);
-  };
-
-  const handleCancelClick = (id) => (event) => {
-    event.stopPropagation();
-    apiRef.current.setRowMode(id, "view");
-
-    const row = apiRef.current.getRow(id);
-    if (row.isNew) {
-      apiRef.current.updateRows([{ id, _action: "delete" }]);
-    }
-  };
-
+export default function AdminGrid({ usersData }) {
   const columns = [
-    { field: "name", headerName: "Name", width: 180, editable: true },
-    { field: "age", headerName: "Age", type: "number", editable: true },
-    {
-      field: "dateCreated",
-      headerName: "Date Created",
-      type: "date",
-      width: 180,
-      editable: true,
-    },
-    {
-      field: "lastLogin",
-      headerName: "Last Login",
-      type: "dateTime",
-      width: 220,
-      editable: true,
-    },
-    {
-      field: "actions",
-      type: "actions",
-      headerName: "Actions",
-      width: 100,
-      cellClassName: "actions",
-      getActions: ({ id }) => {
-        const isInEditMode = apiRef.current.getRowMode(id) === "edit";
-
-        if (isInEditMode) {
-          return [
-            <GridActionsCellItem
-              icon={<SaveIcon />}
-              label="Save"
-              onClick={handleSaveClick(id)}
-              color="primary"
-            />,
-            <GridActionsCellItem
-              icon={<CancelIcon />}
-              label="Cancel"
-              className="textPrimary"
-              onClick={handleCancelClick(id)}
-              color="inherit"
-            />,
-          ];
-        }
-
-        return [
-          <GridActionsCellItem
-            icon={<EditIcon />}
-            label="Edit"
-            className="textPrimary"
-            onClick={handleEditClick(id)}
-            color="inherit"
-          />,
-          <GridActionsCellItem
-            icon={<DeleteIcon />}
-            label="Delete"
-            onClick={handleDeleteClick(id)}
-            color="inherit"
-          />,
-        ];
-      },
-    },
+    { field: "id", headerName: "ID", width: 60 },
+    { field: "nameUser", headerName: "Nombre", width: 130 },
+    { field: "email", headerName: "Email", width: 180 },
+    { field: "phone", headerName: "Teléfono", width: 100 },
+    { field: "createdAt", headerName: "Fecha de alta", width: 150 },
   ];
 
+  const rows = usersData;
+
   return (
-    <Paper
-      sx={{
-        height: 500,
-        width: "100%",
-        "& .actions": {
-          color: "text.secondary",
-        },
-        "& .textPrimary": {
-          color: "text.primary",
-        },
-        zIndex: 1000000,
-      }}
-    >
-      <DataGridPro
-        rows={rows}
-        columns={columns}
-        apiRef={apiRef}
-        editMode="row"
-        onRowEditStart={handleRowEditStart}
-        onRowEditStop={handleRowEditStop}
-        onCellFocusOut={handleCellFocusOut}
-        components={{
-          Toolbar: EditToolbar,
-        }}
-        componentsProps={{
-          toolbar: { apiRef },
-        }}
-      />
-    </Paper>
+    <>
+      {usersData && (
+        <Paper style={{ height: 400, width: "100%" }}>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={10}
+            rowsPerPageOptions={[5]}
+            checkboxSelection
+          />
+        </Paper>
+      )}
+    </>
   );
 }
